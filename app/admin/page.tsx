d3092b0523f95ddda01c58bc56cde8f6b1a0e3b5
Tal.M <t.m.mushkin@gmail.com>
2025-11-07 21:10:45 +0200

    feat: :construction: Some basic styling

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <>
+      <div className="w-full px-2 pt-20">
+        <div className="mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12">
+          <div className=" py-3 lg:gap-0 lg:py-4">
+            <span className="text-xl font-bold">
+              <span className="text-primary">A</span>dmin D
+              <span className="text-primary">A</span>shboard
+            </span>
+            <form className="mt-4">
+              <FieldGroup>
+                <FieldSet>
+                  <FieldLegend>Tags</FieldLegend>
+                  <FieldGroup>
+                    <Field className="border-2 rounded-2xl py-2 px-4">
+                      <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+                        Create a new tag
+                      </FieldLabel>
+                      <FieldDescription>
+                        Tag names must be unique
+                      </FieldDescription>
+                      <div className="flex flex-col w-full gap-4 md:flex-row">
+                        <Input
+                          id="new-tag-name"
+                          placeholder="New tag name"
+                          type="text"
+                          required
+                        />
+
+                        <div className="flex justify-end gap-2">
+                          <Button type="submit">Submit</Button>
+                          <Button variant="outline" type="button">
+                            Clear
+                          </Button>
+                        </div>
+                      </div>
+                    </Field>
+                  </FieldGroup>
+                  <FieldSeparator />
+                  <FieldGroup>
+                    <Field className="border-2 rounded-2xl py-2 px-4">
+                      <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+                        Tags List
+                      </FieldLabel>
+                      <div className="flex flex-col w-full gap-4 md:flex-row">
+                        <p>Something else</p>
+
+                        <div className="flex justify-end gap-2">
+                          <Button type="submit" variant="secondary">
+                            Edit
+                          </Button>
+                          <Button variant="destructive" type="button">
+                            Remove
+                          </Button>
+                        </div>
+                      </div>
+                    </Field>
+                  </FieldGroup>
+                </FieldSet>
+              </FieldGroup>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;
