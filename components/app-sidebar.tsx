cb77eb0fde99ce52469659cdd8fa3e7ae434d6e6
Tal.M <t.m.mushkin@gmail.com>
2025-11-09 11:52:13 +0200

    style: :construction: layout changes

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 
 import {
@@ -13,6 +14,20 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "@radix-ui/react-dialog";
+import { DialogHeader, DialogFooter } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { createCall } from "@/server/calls";
 
 const data = {
   versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
@@ -35,16 +50,62 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [isOpen, setOpen] = useState(false);
+  const [title, setTitle] = useState<string>("");
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  async function handleNewCall(event?: React.FormEvent) {
+    if (event) event.preventDefault();
+    const title = input.trim();
+    if (!title) return;
+    try {
+      const res = await createCall({ title: title });
+      if (res.success) {
+        setInput("");
+        toast.success("Tag created successfully");
+        router.refresh();
+      } else {
+        toast.error(res.message);
+      }
+    } catch (e) {
+      console.error(e);
+      toast.error("Failed to create tag");
+    } finally {
+      setOpen(false);
+    }
+  }
+
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <h2 className="text-xl font-bold">
-          C<span className="text-primary">A</span>lls
-        </h2>
-
-        <div className="flex flex-1 gap-4 justify-center px-2 py-4 mt-20">
-          <Button>New Call</Button>
-        </div>
+        <Dialog open={isOpen} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline">New Call</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <form onSubmit={handleNewCall} className="flex flex-col gap-4">
+              <DialogHeader>
+                <DialogTitle>Create a new Call</DialogTitle>
+                <DialogDescription>
+                  Enter a title for the new Call.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-3">
+                <Input
+                  defaultValue={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Create</Button>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
       </SidebarHeader>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
